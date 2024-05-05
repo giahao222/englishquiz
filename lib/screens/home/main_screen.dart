adc6258e2c51e6d6f2e98d726eae90cb20b89c10
Thanh_Dat <ntd221103@gmail.com>
2024-05-05 17:15:26 +0700

    update UI

--- lib/screens/home/main_screen.dart
+++ lib/screens/home/main_screen.dart
@@ -1,10 +1,9 @@
 import 'package:englishquiz/screens/home/bottom_navbar_controller.dart';
 import 'package:englishquiz/screens/home/home.dart';
+import 'package:englishquiz/screens/library/library.dart';
 import 'package:englishquiz/screens/setting/setting_page.dart';
-import 'package:englishquiz/screens/translate/translate_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:englishquiz/screens/auth/login.dart';
 import 'package:get/get.dart';
 
 class MainScreen extends StatelessWidget {
@@ -13,7 +12,7 @@ class MainScreen extends StatelessWidget {
       Get.put(BottomNavBarController());
   final List<Widget> _pages = [
     const HomePage(),
-    const TranslatePage(),
+    const LibraryPage(),
     const SettingPage()
   ];
   MainScreen({Key? key, required this.user}) : super(key: key);
@@ -27,39 +26,27 @@ class MainScreen extends StatelessWidget {
       body: Obx(() => _pages[bottomNavBarController.currentIndex.value]),
       bottomNavigationBar: Obx(
         () => BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
           currentIndex: bottomNavBarController.currentIndex.value,
           onTap: (index) {
             bottomNavBarController.changeIndex(index);
           },
           items: [
-            const BottomNavigationBarItem(
+            BottomNavigationBarItem(
               icon: Icon(Icons.home),
-              label: 'Trang chủ',
+              label: 'Home',
             ),
-            const BottomNavigationBarItem(
-              icon: Icon(Icons.translate),
-              label: 'Dịch',
+            BottomNavigationBarItem(
+              icon: Icon(Icons.folder_open_rounded),
+              label: 'Library',
             ),
-            const BottomNavigationBarItem(
+            BottomNavigationBarItem(
               icon: Icon(Icons.settings),
-              label: 'Cài đặt',
+              label: 'Settings',
             ),
           ],
         ),
       ),
     );
-  }
-
-  Future<void> _signOut(BuildContext context) async {
-    try {
-      await FirebaseAuth.instance.signOut();
-      print("Logout successful");
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => LoginPage()),
-      );
-    } catch (e) {
-      print(e.toString());
-    }
   }
 }
---
+++ lib/screens/library/library.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LibraryPage extends StatelessWidget {
+  const LibraryPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Library Page'),
+    );
+  }
+}
