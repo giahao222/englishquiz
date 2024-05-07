71918a8a1f4053f6cdc6e67c18a06ab496b0a0ab
Thanh_Dat <ntd221103@gmail.com>
2024-05-08 00:25:07 +0700

    add add topics feature

--- lib/screens/library/AddTopic.dart
+++ lib/screens/library/AddTopic.dart
@@ -4,42 +4,44 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+class WordPair {
+  String eng;
+  String vie;
+
+  WordPair({required this.eng, required this.vie});
+}
+
 class AddTopicController extends GetxController {
   var name = ''.obs;
   var image = ''.obs;
   var isPublic = false.obs;
   var isEngType = false.obs;
   var listVocab = <String, dynamic>{}.obs;
+  var topics = <WordPair>[].obs;
   final _creator = FirebaseAuth.instance.currentUser!.displayName;
 
-  var nameController = TextEditingController();
-  var idError = Rx<String?>(null);
-  var nameError = Rx<String?>(null);
 
   @override
   void onInit() {
     super.onInit();
-    nameController.addListener(validateName);
   }
 
   void uploadTopic() {
-    validateName();
-    if (nameError.value != null) return;
-    final id = UniqueKeyGenerator().generateId();
   }
 
-  void validateName() {
-    if (nameController.text.isEmpty) {
-      nameError.value = "Name cannot be empty";
-    } else {
-      nameError.value = null;
+  void addCard() {
+    topics.add(WordPair(eng: '', vie: ''));
+  }
+
+  void removeCard(int index) {
+    topics.removeAt(index);
+  }
+
+  String? textValidator(String value) {
+    if (value.isEmpty) {
+      return 'This field is required';
     }
-  }
-
-  @override
-  void onClose() {
-    nameController.dispose();
-    super.onClose();
+    return null;
   }
 }
 
@@ -54,37 +56,103 @@ class AddTopic extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              onChanged: (value) => controller.name.value = value,
-              decoration: InputDecoration(labelText: 'Name'),
-            ),
-            TextField(
-              onChanged: (value) => controller.image.value = value,
-              decoration: InputDecoration(labelText: 'Image URL'),
-            ),
-            Obx(() => SwitchListTile(
-                  title: Text("Is Public?"),
-                  value: controller.isPublic.value,
-                  onChanged: (value) => controller.isPublic.value = value,
-                )),
-            Obx(() => SwitchListTile(
-                  title: Text("Is English Type?"),
-                  value: controller.isEngType.value,
-                  onChanged: (value) => controller.isEngType.value = value,
-                )),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: controller.uploadTopic,
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
-                fixedSize: MaterialStateProperty.all(Size.fromWidth(200)),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                onChanged: (value) => controller.name.value = value,
+                decoration: InputDecoration(labelText: 'Name'),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) => controller.textValidator(value!),
               ),
-              child:
-                  Text('Upload Topic', style: TextStyle(color: Colors.white)),
-            ),
-          ],
+              TextField(
+                onChanged: (value) => controller.image.value = value,
+                decoration: InputDecoration(labelText: 'Image URL'),
+              ),
+              Obx(() => SwitchListTile(
+                    title: Text("Is Public?"),
+                    value: controller.isPublic.value,
+                    onChanged: (value) => controller.isPublic.value = value,
+                  )),
+              Obx(() => SwitchListTile(
+                    title: Text("Is English Type?"),
+                    value: controller.isEngType.value,
+                    onChanged: (value) => controller.isEngType.value = value,
+                  )),
+              Container(
+                height: 2,
+                margin: EdgeInsets.symmetric(vertical: 25),
+                color: Colors.black,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('List Vocabulary'),
+                  IconButton(
+                    tooltip: 'Import from csv file',
+                    onPressed: () {
+                    },
+                    icon: Icon(Icons.import_contacts_rounded),
+                  ),
+                ],
+              ),
+              Obx(
+                () => Column(
+                  children: List.generate(
+                      controller.topics.length,
+                      (index) => Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Column(
+                                children: [
+                                  TextField(
+                                    onChanged: (value) =>
+                                        controller.topics[index].eng = value,
+                                    decoration:
+                                        InputDecoration(labelText: 'English'),
+                                  ),
+                                  TextField(
+                                    onChanged: (value) =>
+                                        controller.topics[index].vie = value,
+                                    decoration:
+                                        InputDecoration(labelText: 'Vietnamese'),
+                                  ),
+                                  if (controller.topics.length > 1)
+                                    IconButton(
+                                      onPressed: () =>
+                                          controller.removeCard(index),
+                                      icon: Icon(Icons.delete),
+                                      color: Colors.red,
+                                    ),
+                                ],
+                              ),
+                            ),
+                          )),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  controller.addCard();
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green),
+                  fixedSize: MaterialStateProperty.all(Size.fromWidth(100)),
+                ),
+                child: Icon(Icons.add, color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: controller.uploadTopic,
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
+                  fixedSize: MaterialStateProperty.all(Size.fromWidth(200)),
+                ),
+                child:
+                    Text('Upload Topic', style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
         ),
       ),
     );
