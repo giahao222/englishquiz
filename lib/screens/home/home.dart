3ef23a88202fb5d5dc6cd04c19f0979a3d245b41
Thanh_Dat <ntd221103@gmail.com>
2024-05-12 21:55:26 +0700

    home_ui

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,53 +1,166 @@
+import 'dart:ui';
+
+import 'package:englishquiz/models/Topic.dart';
+import 'package:englishquiz/screens/library/MyTopics.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:englishquiz/screens/auth/login.dart';
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  var isExpanded = {}.obs;
+  var publicTopics = <Topic>[].obs;
+  final TopicController topicController = Get.find();
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchPublicTopics();
+    isExpanded.value = {
+      'listOfTopics': false,
+      'publicTopics': false,
+    };
+
+    ever(topicController.topics, (_) {
+      fetchPublicTopics();
+    });
+  }
+
+  void fetchPublicTopics() {
+    publicTopics.value = topicController.topics
+        .where((element) => element.isPublic)
+        .toList() as List<Topic>;
+  }
+}
 
 class HomePage extends StatelessWidget {
-  final User user;
-  final _creator = FirebaseAuth.instance.currentUser!.displayName;
-
-  HomePage({Key? key, required this.user}) : super(key: key);
-
+  HomePage({super.key});
+  final TopicController topicController = Get.find();
+  final HomeController homeController = Get.put(HomeController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Trang chủ'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (user.email != null)  // Kiểm tra user không null và email không null
-              Text(
-                'Xin chào, ${_creator}!',
-                style: TextStyle(fontSize: 20.0),
-              ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                _signOut(context);
-              },
-              child: Text('Đăng xuất'),
-            ),
-          ],
+      body: SafeArea(
+        child: Obx(
+          () => Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text('Topics'),
+                    trailing: IconButton(
+                      icon: Icon(homeController.isExpanded['listOfTopics']
+                          ? Icons.expand_less
+                          : Icons.expand_more),
+                      onPressed: () {
+                        homeController.isExpanded['listOfTopics'] =
+                            !homeController.isExpanded['listOfTopics'];
+                        homeController.isExpanded.refresh();
+                      },
+                    ),
+                  ),
+                  Visibility(
+                    visible: homeController.isExpanded['listOfTopics'],
+                    child: GridView.builder(
+                      // Replace GridView.builder with ListView.builder
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: topicController.topics.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 5,
+                        crossAxisSpacing: 5,
+                        mainAxisSpacing: 5,
+                      ),
+                      itemBuilder: (context, index) {
+                        return TopicItemCard(
+                          topic: topicController.topics[index],
+                          fontSize: 12,
+                        );
+                      },
+                    ),
+                  ),
+                  Visibility(
+                    visible: !homeController.isExpanded['listOfTopics'],
+                    child: SizedBox(
+                      height: 100,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: topicController.topics.length,
+                        itemBuilder: (context, index) {
+                          return SizedBox(
+                            width: 100,
+                            child: TopicItemCard(
+                              topic: topicController.topics[index],
+                              fontSize: 12,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('Public Topics'),
+                    trailing: IconButton(
+                      icon: Icon(homeController.isExpanded['publicTopics']
+                          ? Icons.expand_less
+                          : Icons.expand_more),
+                      onPressed: () {
+                        homeController.isExpanded['publicTopics'] =
+                            !homeController.isExpanded['publicTopics'];
+                        homeController.isExpanded.refresh();
+                      },
+                    ),
+                  ),
+                  Visibility(
+                    visible: homeController.isExpanded['publicTopics'],
+                    child: GridView.builder(
+                      // Replace GridView.builder with ListView.builder
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: homeController.publicTopics.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 5,
+                        crossAxisSpacing: 5,
+                        mainAxisSpacing: 5,
+                      ),
+                      itemBuilder: (context, index) {
+                        return TopicItemCard(
+                          topic: homeController.publicTopics[index],
+                          fontSize: 12,
+                        );
+                      },
+                    ),
+                  ),
+                  Visibility(
+                    visible: !homeController.isExpanded['publicTopics'],
+                    child: SizedBox(
+                      height: 100,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: homeController.publicTopics.length,
+                        itemBuilder: (context, index) {
+                          return SizedBox(
+                            width: 100,
+                            child: TopicItemCard(
+                              topic: homeController.publicTopics[index],
+                              fontSize: 12,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              )),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          print(homeController.publicTopics);
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
-
-  Future<void> _signOut(BuildContext context) async {
-    try {
-      await FirebaseAuth.instance.signOut();
-      print("Logout successful");
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => LoginPage()),
-      );
-    } catch (e) {
-      print(e.toString());
-    }
-  }
-
 }
